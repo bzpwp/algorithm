7aa1d8374d0e6bab37256d1d4d928d8acd663bf7
bzpwp <135491877+bzpwp@users.noreply.github.com>
2023-10-11 15:52:21 +0900

    Update 座標圧縮.py

--- algorithm/座標圧縮.py
+++ algorithm/座標圧縮.py
@@ -1,3 +1,6 @@
+'''
+1次元
+'''
 # coding: utf-8
 
 # 座標圧縮したい数列
@@ -12,3 +15,31 @@ D = { v: i for i, v in enumerate(B) }
 
 # 答え
 print(list(map(lambda v: D[v], A)))
+
+'''
+2次元
+'''
+def compress(C1, C2):
+    vals = []
+    N = len(C1)
+    
+    for i in range(N):
+        for d in range(2):  # その位置と、一つ隣を確保(隣を確保しないと空白が埋まってしまうことがある)
+            tc1 = C1[i] + d
+            tc2 = C2[i] + d
+            vals.append(tc1)
+            vals.append(tc2)
+    
+    # ソート
+    vals.sort()
+    
+    # 隣り合う重複を削除
+    vals = list(set(vals))
+    vals.sort()
+    
+    for i in range(N):
+        C1[i] = vals.index(C1[i])
+        C2[i] = vals.index(C2[i])
+    
+    return vals
+
