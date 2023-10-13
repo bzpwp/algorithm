f4fd7220f16a8d7876f0e0250d05e5012b985aa9
bzpwp <135491877+bzpwp@users.noreply.github.com>
2023-10-13 18:27:10 +0900

    Update math.py
    add 行列累乗

--- algorithm/math/math.py
+++ algorithm/math/math.py
@@ -167,3 +167,54 @@ def pow_k(x, n, mod):
         n //= 2
 
     return K * x
+
+# 行列累乗
+
+
+
+def mat_mul(a, b) :
+    I, J, K = len(a), len(b[0]), len(b)
+    c = [[0] * J for _ in range(I)]
+    for i in range(I) :
+        for j in range(J) :
+            for k in range(K) :
+                c[i][j] += a[i][k] * b[k][j]
+            c[i][j] %= m
+    return c
+
+
+def mat_pow(x, n):
+    y = [[0] * len(x) for _ in range(len(x))]
+
+    for i in range(len(x)):
+        y[i][i] = 1
+
+    while n > 0:
+        if n & 1:
+            y = mat_mul(x, y)
+        x = mat_mul(x, x)
+        n >>= 1
+
+    return y
+
+
+l, a, b, m = LI()
+d0 = 0
+ret = [[0], [a], [1]]
+for i in range(1, 19):
+    if 10 ** i - 1 - a < 0:
+        continue
+    d1 = min((10 ** i - 1 - a) // b + 1, l)
+    mat = [[10 ** i, 1, 0], [0, 1, b], [0, 0, 1]]
+    ret = mat_mul(mat_pow(mat, d1 - d0), ret)
+    if d1 == l:
+        break
+    d0 = d1
+
+
+//　行列計算の順番によって答えが変わる
+// 上の例だと、正方形の右側に縦一列の行列がある
+// 正方形の中で入力が横、出力が縦のイメージ。行列の順番が逆
+//　だと逆になる
+
+print(ret[0][0])
