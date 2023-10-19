63aef8173db9f240beb4a7622ee83f882631b506
bzpwp <135491877+bzpwp@users.noreply.github.com>
2023-10-19 17:56:18 +0900

    add doubling and 木の直径

--- algorithm/dfs.py
+++ algorithm/dfs.py
@@ -300,3 +300,39 @@ answer *= power_inv[1]
 answer %= MOD
 print(answer)
 
+
+
+
+
+
+
+'''
+木の直径
+'''
+n = int(input())
+from collections import defaultdict
+dd = defaultdict(list)
+for i in range(n-1):
+    a,b = map(int,input().split())
+    dd[a].append(b)
+    dd[b].append(a)
+
+visited = set([])
+length = ["" for i in range(n)]
+u = 0
+def dfs(p,check,dist):
+    global length, u
+    length[p-1] = dist
+    if dist >= u:
+        u = p
+    # dist += 1
+    check.add(p)
+    for next in dd[p]:
+        if next not in check:
+            dfs(next, check, dist+1)
+
+dfs(1,visited,0)
+visited = set([])
+length = ["" for i in range(n)]
+dfs(u,visited,0)
+print(max(length))
